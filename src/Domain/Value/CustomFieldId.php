9b17db84348bca6c3a2b16c9236ede104436becf
Oskar Stark <oskarstark@googlemail.com>
2024-01-04 11:48:46 +0100

    Add `CustomFieldId`

---
+++ src/Domain/Value/CustomFieldId.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Iusta-Api.
+ *
+ * (c) Datana GmbH <info@datana.rocks>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Datana\Iusta\Api\Domain\Value;
+
+use Webmozart\Assert\Assert;
+
+final readonly class CustomFieldId
+{
+    public function __construct(
+        public int $value,
+    ) {
+        Assert::greaterThan($value, 0);
+    }
+
+    public static function fromString(string $value): self
+    {
+        return new self((int) str_replace('cf_', '', $value));
+    }
+
+    public function toInt(): int
+    {
+        return $this->value;
+    }
+
+    public function toString(): string
+    {
+        return 'cf_' . $this->value;
+    }
+}
