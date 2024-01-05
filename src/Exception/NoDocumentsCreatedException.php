a012d3d21cfaf8a0e4e333a0c7082c370f975d66
Oskar Stark <oskarstark@googlemail.com>
2024-01-05 07:15:33 +0100

    Enhancement: Add exceptions (#21)

---
+++ src/Exception/NoDocumentsCreatedException.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Iusta-Api.
+ *
+ * (c) Datana GmbH <info@datana.rocks>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Datana\Iusta\Api\Exception;
+
+/**
+ * @author Oskar Stark <oskarstark@googlemail.com>
+ */
+final class NoDocumentsCreatedException extends \RuntimeException
+{
+    public function __construct()
+    {
+        parent::__construct('No documents created.');
+    }
+}
