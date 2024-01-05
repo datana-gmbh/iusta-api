fad493eba42e4858f1d24edd2f0e1ec34fa9d6a0
Oskar Stark <oskarstark@googlemail.com>
2024-01-05 21:51:54 +0100

    Enhancement: Add `DatasetApi` (#27)

---
+++ src/DatasetApiInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Iusta-Api.
+ *
+ * (c) Datana GmbH <info@datana.rocks>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Datana\Iusta\Api;
+
+use Datana\Iusta\Api\Domain\Value\CreatedDataset;
+use Datana\Iusta\Api\Domain\Value\DatasetTypeId;
+
+/**
+ * @author Oskar Stark <oskar.stark@googlemail.de>
+ */
+interface DatasetApiInterface
+{
+    /**
+     * @param list<array{id: int, value: mixed}> $customFieldValues
+     */
+    public function create(string $name, DatasetTypeId $datasetTypeId, array $customFieldValues = []): CreatedDataset;
+}
---
+++ tests/Unit/Domain/Value/DatasetIdTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Iusta-Api.
+ *
+ * (c) Datana GmbH <info@datana.rocks>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Datana\Iusta\Api\Tests\Unit\Domain\Value;
+
+use Datana\Iusta\Api\Domain\Value\DatasetId;
+use Ergebnis\Test\Util\Helper;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Datana\Iusta\Api\Domain\Value\DatasetId
+ */
+final class DatasetIdTest extends TestCase
+{
+    use Helper;
+
+    /**
+     * @test
+     *
+     * @dataProvider \Ergebnis\Test\Util\DataProvider\IntProvider::greaterThanZero()
+     */
+    public function canBeConstructed(int $value): void
+    {
+        $id = new DatasetId($value);
+
+        self::assertSame($value, $id->value);
+        self::assertSame($value, $id->toInt());
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider \Ergebnis\Test\Util\DataProvider\IntProvider::lessThanZero()
+     * @dataProvider \Ergebnis\Test\Util\DataProvider\IntProvider::zero()
+     */
+    public function throwsException(int $value): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        new DatasetId($value);
+    }
+}
