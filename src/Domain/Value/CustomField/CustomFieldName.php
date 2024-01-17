434e39575768490acbed80130de250380458a0c2
Oskar Stark <oskarstark@googlemail.com>
2024-01-17 07:59:39 +0100

    Add `Shortcode` and `CustomFieldName`

---
+++ src/Domain/Value/CustomField/CustomFieldName.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Iusta-Api.
+ *
+ * (c) Datana GmbH <info@datana.rocks>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Datana\Iusta\Api\Domain\Value\CustomField;
+
+use OskarStark\Value\TrimmedNonEmptyString;
+
+final readonly class CustomFieldName
+{
+    public function __construct(
+        public string $value,
+    ) {
+        TrimmedNonEmptyString::fromString($value);
+    }
+
+    public function toString(): string
+    {
+        return $this->value;
+    }
+}
