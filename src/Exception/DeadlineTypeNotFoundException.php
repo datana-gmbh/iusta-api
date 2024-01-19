117ae572b19b375f7f99ffd4c82922a404e9d14a
Oskar Stark <oskarstark@googlemail.com>
2024-01-19 20:16:08 +0100

    Add `DeadlineTypeApi` (#55)

---
+++ src/Exception/DeadlineTypeNotFoundException.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Iusta-Api.
+ *
+ * (c) Datana GmbH <info@datana.rocks>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Datana\Iusta\Api\Exception;
+
+use Datana\Iusta\Api\Domain\Value\DeadlineType\DeadlineTypeName;
+
+/**
+ * @author Oskar Stark <oskarstark@googlemail.com>
+ */
+final class DeadlineTypeNotFoundException extends \RuntimeException
+{
+    public static function forName(DeadlineTypeName $name): self
+    {
+        return new self(sprintf(
+            'DeadlineType with name "%s" not found.',
+            $name->toString(),
+        ));
+    }
+}
