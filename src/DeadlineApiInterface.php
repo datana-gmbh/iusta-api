691412e3af515b1aec2da0d63a6f3c7dd994d9fd
Oskar Stark <oskarstark@googlemail.com>
2024-01-24 15:19:35 +0100

    Enhancement: Add `DeadlineApi`

---
+++ src/DeadlineApiInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Iusta-Api.
+ *
+ * (c) Datana GmbH <info@datana.rocks>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Datana\Iusta\Api;
+
+use Datana\Iusta\Api\Domain\Value\Case\CaseId;
+use Datana\Iusta\Api\Domain\Value\Deadline\Deadline;
+use Datana\Iusta\Api\Domain\Value\Deadline\DeadlineName;
+use Datana\Iusta\Api\Domain\Value\Deadline\Status;
+use Datana\Iusta\Api\Domain\Value\DeadlineType\DeadlineType;
+use Datana\Iusta\Api\Domain\Value\DeadlineType\DeadlineTypeName;
+
+/**
+ * @author Oskar Stark <oskar.stark@googlemail.de>
+ */
+interface DeadlineApiInterface
+{
+    public function create(CaseId $caseId, \DateTimeImmutable $dueAt, DeadlineType $deadlineType, Status $status = Status::Open, ?\DateTimeImmutable $preDueAt = null, ?DeadlineName $name = null, ?string $comment = null): Deadline;
+}
