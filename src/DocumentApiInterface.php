c164227a25a4921466ea8adfb21a8e2cac2ec0f2
Oskar Stark <oskarstark@googlemail.com>
2024-01-26 10:59:41 +0100

    Enhancement: Add DocumentApi

---
+++ src/DocumentApiInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Iusta-Api.
+ *
+ * (c) Datana GmbH <info@datana.rocks>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Datana\Iusta\Api;
+
+use Datana\Iusta\Api\Domain\Value\Document\DocumentId;
+
+/**
+ * @author Oskar Stark <oskar.stark@googlemail.de>
+ */
+interface DocumentApiInterface
+{
+    public function update(DocumentId $id, ): Documen
+}
--- src/Domain/Value/Document/Document.php
+++ src/Domain/Value/Document/Document.php
@@ -13,19 +13,31 @@ declare(strict_types=1);
 
 namespace Datana\Iusta\Api\Domain\Value\Document;
 
+use Datana\Iusta\Api\Domain\Value\DocumentCategory\DocumentCategoryId;
 use Webmozart\Assert\Assert;
 
 final readonly class Document
 {
     public DocumentId $id;
+    public DocumentCategoryId $documentCategoryId;
 
     /**
-     * @param array<mixed> $values
+     * @param array{createdDocument: Values}|Values $values
      */
     public function __construct(
         public array $values,
     ) {
+        if (\array_key_exists('createdDocument', $values)) {
+            Assert::notEmpty($values['createdDocument']);
+            $values = $values['createdDocument'];
+        }
+
         Assert::keyExists($values, 'id');
+        Assert::integer($values['id']);
         $this->id = new DocumentId($values['id']);
+
+        Assert::keyExists($values, 'documentCategoryId');
+        Assert::integer($values['documentCategoryId']);
+        $this->documentCategoryId
     }
 }
